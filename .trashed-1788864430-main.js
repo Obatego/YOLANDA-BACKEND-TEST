3d4739713ffcab8973f9d40de412418848d151c4
KingsCode <166356948+Obatego@users.noreply.github.com>
2026-08-31 12:27:27 +0100

    Delete .trashed-1788864430-main.js

--- .trashed-1788864430-main.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const splash=document.querySelector('.splash');
-  if(splash){
-    let closed=false;
-    const close=()=>{if(closed)return;closed=true;splash.classList.add('hide');setTimeout(()=>splash.remove(),560)};
-    splash.addEventListener('pointerdown',close,{passive:true});
-    setTimeout(close,2500);
-  }
-})();
